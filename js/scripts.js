c326ce4a4cb4db36f3e1362e421bd36df18b2c1e
Emily Chadwick <ekaseberg@gmail.com>
2017-01-01 14:13:44 -0800

    Working Calculator

--- js/scripts.js
+++ js/scripts.js
@@ -4,22 +4,36 @@ $(document).ready(function() {
 
   $('.integer').on('click', function () {
     entered += this.innerText;
-    console.log(entered);
-
+    // console.log(entered);
     $('.screen').text(entered);
 
   });
 
   $('.operator').on('click', function () {
-    entered += this.innerText;
-    console.log(entered);
 
-    $('.screen').text(entered);
+    if (entered[entered.length - 1] === '-' ||
+        entered[entered.length - 1] === '+' ||
+        entered[entered.length - 1] === '*' ||
+        entered[entered.length - 1] === '/') {
 
+      // replace old operator with new
+
+      entered = entered.split("");
+      entered[entered.length - 1] = this.innerText;
+      entered = entered.join('');
+      $('.screen').text(entered);
+
+    } else {
+
+      // add like normal
+      entered += this.innerText;
+      $('.screen').text(entered);
+
+    }
   });
 
   $('.calculate').on('click', function () {
-    console.log(entered);
+    // console.log(entered);
     if (entered.indexOf('/0') !== -1) {
       $('.screen').text('Error');
     } else {
@@ -28,20 +42,11 @@ $(document).ready(function() {
     };
   });
 
-
-
-
-
-
-    //eval(entered);
-    //$('.screen').text(eval(entered));
-
-
   $('.clear').on('click', function () {
     entered = "";
     $('.screen').text(entered);
   });
 
-
-
 });
+
+
